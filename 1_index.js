0a33eee428c24e528f8f905104624e4efd92b261
Amresh04 <kumaramresh0174@gmail.com>
2026-06-30 10:33:36 +0530

    JavaScript

---
+++ 1_index.js
@@ -0,0 +1,19 @@
+// Variable making
+let name="Amresh kumar"; //we can not make same variable in multipletime
+let age=20;
+age=30;
+console.log(name,age);
+
+
+const account=1234;
+
+//  value can not change of const variable
+
+console.log(account);
+
+
+// old variable making method(we can make same variable in multiple time)
+
+var a=49;
+var a=90;
+console.log(a)
