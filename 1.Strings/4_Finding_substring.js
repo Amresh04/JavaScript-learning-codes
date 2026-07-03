ad24148206b0c6b31d293451aa15fd16feb901b2
Amresh04 <kumaramresh0174@gmail.com>
2026-07-03 12:47:04 +0530

    JavaScript

---
+++ 1.Strings/4_Finding_substring.js
@@ -0,0 +1,13 @@
+const str='Hello everyone Welcome to the JavaScript Learning Phase JavaScript';
+console.log(str);
+
+console.log(str.indexOf('Learning'));
+
+console.log(str.lastIndexOf('JavaScript'));
+
+
+console.log(str.includes('Amresh'));
+
+
+
+console.log(str.substring(2,5))
