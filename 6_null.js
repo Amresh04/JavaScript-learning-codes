dc75f95b19a778172b845a472c8929a0a0979927
Amresh04 <kumaramresh0174@gmail.com>
2026-07-07 08:50:15 +0530

    JavaScript

---
+++ 6_null.js
@@ -0,0 +1,15 @@
+console.log(null==undefined);
+console.log(null===undefined);
+console.log(null==0);
+console.log(null=="");
+console.log(null==false);
+console.log(null==true);
+
+
+
+//>,<,<=,>=(null---->Number)
+
+console.log(null>=0);
+console.log(null<=0);
+console.log(null>0);
+console.log(null<0);
