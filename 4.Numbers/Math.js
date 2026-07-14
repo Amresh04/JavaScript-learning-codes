55fb6e5750b41f6b15cd8639513e3ef76beb18d5
Amresh04 <kumaramresh0174@gmail.com>
2026-07-14 17:59:02 +0530

    JavaScript

---
+++ 4.Numbers/Math.js
@@ -0,0 +1,8 @@
+console.log(Math.abs(-4));
+console.log(Math.PI);
+console.log(Math.log10);
+console.log(Math.E);
+console.log(Math.SQRT1_2);
+console.log(Math.LOG2E);
+console.log(Math.floor(2.3));//give lower value
+console.log(Math.ceil(2.3));//give upper value
