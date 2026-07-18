fae179b20043194bdc3461f449fcf7b7b737d97e
Amresh04 <kumaramresh0174@gmail.com>
2026-07-18 10:00:55 +0530

    JavaScript

---
+++ 5.operators/3_comparision.js
@@ -0,0 +1,10 @@
+let x=20;
+let y=30;
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x==y);
+console.log(x===y); 
+
+// number and string compare(string convert in number_)
