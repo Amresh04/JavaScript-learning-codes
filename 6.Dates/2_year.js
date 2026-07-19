27ced1d61783ca9b49c83124a37e332e9e749f06
Amresh04 <kumaramresh0174@gmail.com>
2026-07-19 16:17:30 +0530

    Javascript

---
+++ 6.Dates/2_year.js
@@ -0,0 +1,6 @@
+const now=new Date();
+console.log(now.getDate());
+console.log(now.getFullYear());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getMilliseconds());
