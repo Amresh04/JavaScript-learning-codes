3a6da88b997916c907fff73348c5445df41b13a7
Amresh04 <kumaramresh0174@gmail.com>
2026-07-20 10:14:05 +0530

    JavaScript

---
+++ 6.Dates/4_Time_stamp.js
@@ -0,0 +1,7 @@
+const now=Date.now();
+const dates=new Date(now);
+console.log(now);
+console.log(dates)
+
+//Time in milliSecond:1782629923768(start calculated from 1 jan 1970)
+//UTC-Time:2026-06-28T06:59:54.263Z
