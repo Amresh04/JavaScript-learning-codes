4128c2121a627df95d0e5529a985f124acd51fe9
Amresh04 <kumaramresh0174@gmail.com>
2026-07-24 09:23:06 +0530

    JavaScript

---
+++ 7.Arrays/5_delete.js
@@ -0,0 +1,11 @@
+let arr=[20,30,50,70,90];
+arr.pop(90);
+console.log(arr);
+
+//starting add 
+arr.unshift("Aayush",100000);
+console.log(arr);
+
+//delete from staring
+arr.shift();
+console.log(arr);
