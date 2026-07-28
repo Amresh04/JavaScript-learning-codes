39576ad8eeefcbc14bb093179e20bf801f67b731
Amresh04 <kumaramresh0174@gmail.com>
2026-07-28 12:11:10 +0530

    JavaScript

---
+++ 8.Object copy 2/6_Keys.js
@@ -0,0 +1,9 @@
+const user={
+    name:"Amresh",
+    age:20,
+    branch:"CSE(AI & ML)",
+    salary:"15 lakh/Y"
+}
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
