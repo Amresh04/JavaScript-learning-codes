22266f823cb68f0053df4ac8c1f42842b8394599
Amresh04 <kumaramresh0174@gmail.com>
2026-07-30 00:59:49 +0530

    JavaScript

---
+++ 8.Object copy 2/9_function_in _object.js
@@ -0,0 +1,10 @@
+const user={
+    name:"Amresh",
+    age:20,
+    branch:"CSE(AI & ML)",
+salary:"15 lakh/Y",
+greeting: function(){
+    console.log(`Welcome to JavaScript of learning ${user.name}`);
+}
+}   
+user.greeting(); 
