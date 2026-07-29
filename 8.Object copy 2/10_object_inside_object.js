38135c382ff0d74afb02d94eb914dd68d61ac3c0
Amresh04 <kumaramresh0174@gmail.com>
2026-07-30 01:00:09 +0530

    JavaScript

---
+++ 8.Object copy 2/10_object_inside_object.js
@@ -0,0 +1,12 @@
+const user={
+    name:"Amresh",
+    age:20,
+    branch:"CSE(AI & ML)",
+    address:{
+        "village":"Mahuara",
+        "city":"Muzaffarpur"
+    }
+    
+}
+console.log(user.address);
+console.log(user.address.city);
