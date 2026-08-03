8a4c2023c568b49b8400d6fcceb09ddae1f2a67d
Amresh04 <kumaramresh0174@gmail.com>
2026-08-03 19:03:36 +0530

    JavaScript

---
+++ 9.Functions/5_3rd_type-function.js
@@ -0,0 +1,12 @@
+// arrow function
+// ()=>{
+
+// }
+
+// const addNumber=(a,b)=>{
+//     return a+b;
+// }
+// console.log(addNumber(20,30));
+
+const addNumber=(a,b)=>a+b;
+console.log(addNumber(30,80));
